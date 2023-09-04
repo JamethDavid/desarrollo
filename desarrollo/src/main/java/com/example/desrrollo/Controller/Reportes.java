a7bef0f0b9f18cd2b5af04328612104f73ff69af
Jameth <jdmelgarejo@uniboyaca.edu.co>
2023-09-04 18:01:32 -0500

    Revisar que dice que no class path resource [AuxilioInventario.jrxml] cannot be resolved to absolute file path because it does not exist

--- desarrollo/src/main/java/com/example/desrrollo/Controller/Reportes.java
+++ desarrollo/src/main/java/com/example/desrrollo/Controller/Reportes.java
@@ -1,4 +1,21 @@
 package com.example.desrrollo.Controller;
+
+import com.example.desrrollo.Services.ReporteService;
+import net.sf.jasperreports.engine.JRException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileNotFoundException;
+
+@RestController
+@RequestMapping("/api")
 public class Reportes {
+@Autowired
+    private ReporteService reporteService;
+
+@GetMapping("/report/{format}")
+    public String generaRepor(@PathVariable String format) throws JRException, FileNotFoundException {
+    return reporteService.exportReport(format);
+}
 
 }
