63fc6cfd5cf48c6d140e9e7fbe368fb519aa06a3
JamethDavid <112991287+JamethDavid@users.noreply.github.com>
2023-10-20 11:45:53 -0500

    zuna lista hecha

---
+++ desarrollo/src/main/java/com/example/desrrollo/Entity/Zona.java
@@ -0,0 +1,2 @@
+package com.example.desrrollo.Entity;public class Zona {
+}
---
+++ desarrollo/src/main/java/com/example/desrrollo/Query/ReferenciaZonaDTO.java
@@ -0,0 +1,16 @@
+package com.example.desrrollo.Query;
+
+public class ReferenciaZona {
+    private String nombre;
+    public ReferenciaZona() {
+    }
+    public ReferenciaZona(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
