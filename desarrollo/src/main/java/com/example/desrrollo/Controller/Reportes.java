0560f7901953f4aef50bd8a6426e8804ee93b54c
JamethDavid <112991287+JamethDavid@users.noreply.github.com>
2023-11-16 11:45:20 -0500

    token ya en reprotes

--- desarrollo/src/main/java/com/example/desrrollo/Controller/Reportes.java
+++ desarrollo/src/main/java/com/example/desrrollo/Controller/Reportes.java
@@ -24,10 +24,14 @@ private  IReporteService IreporteService;
     protected ObjectMapper mapper;
     @Autowired
     private DataSourceMap dataSources;
-    @GetMapping("/auxilio-inventario-pdf")
-    public ResponseEntity<byte[]> exportPdf() throws JRException, FileNotFoundException {
+    @GetMapping("/auxilio-inventario-pdf/{token}")
+    public ResponseEntity<byte[]> exportPdf(@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportPdf());
     }
     @GetMapping("/lista-precio-pdf/{token}")
@@ -40,100 +44,154 @@ private  IReporteService IreporteService;
         }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToListaPrecioPdf());
     }
-    @GetMapping("/lista-existente-pdf")
-    public ResponseEntity<byte[]> exportToListaExistentePdf() throws JRException, FileNotFoundException {
+    @GetMapping("/lista-existente-pdf/{token}")
+    public ResponseEntity<byte[]> exportToListaExistentePdf(@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToListaExistentePdf());
     }
-    @GetMapping("/lista-inventario-valorizado-pdf")
-    public ResponseEntity<byte[]> exportToInventarioValorizadoPdf() throws JRException, FileNotFoundException {
+    @GetMapping("/lista-inventario-valorizado-pdf/{token}")
+    public ResponseEntity<byte[]> exportToInventarioValorizadoPdf(@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToInventarioValorizadoPdf());
     }
-    @GetMapping("/reporte-pedido-pendiente-pdf")
-    public ResponseEntity<byte[]> exporToReportePedidoPendientePdf() throws JRException, FileNotFoundException {
+    @GetMapping("/reporte-pedido-pendiente-pdf/{token}")
+    public ResponseEntity<byte[]> exporToReportePedidoPendientePdf(@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToReportePedidoPendientePdf());
     }
 
-    @GetMapping("/lista-producto-pdf/{nombre}")
-    public ResponseEntity<byte[]> exportToListaProductoPdf(@PathVariable String nombre) throws JRException, FileNotFoundException {
+    @GetMapping("/lista-producto-pdf/{nombre}/{token}")
+    public ResponseEntity<byte[]> exportToListaProductoPdf(@PathVariable String nombre,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToLineaProductoPdf(nombre));
     }
-    @GetMapping("/entrada-inventario-pdf/{fechaInicio}/{fechaFinal}")
+    @GetMapping("/entrada-inventario-pdf/{fechaInicio}/{fechaFinal}/{token}")
     public ResponseEntity<byte[]> exportToEntradaInventario(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio
-            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal) throws JRException, FileNotFoundException {
+            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal
+            ,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToEntradaInventarioPdf(fechaInicio,fechaFinal));
     }
-    @GetMapping("/salida-inventario-pdf/{fechaInicio}/{fechaFinal}")
+    @GetMapping("/salida-inventario-pdf/{fechaInicio}/{fechaFinal}/{token}")
     public ResponseEntity<byte[]> exportToSalidaInventario(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio
-            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal) throws JRException, FileNotFoundException {
+            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal
+            ,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToSalidaInventarioPdf(fechaInicio,fechaFinal));
     }
-
-
-    @GetMapping("/producto-rentabilidad-pdf/{fechaInicio}/{fechaFinal}")
+    @GetMapping("/producto-rentabilidad-pdf/{fechaInicio}/{fechaFinal}/{token}")
     public ResponseEntity<byte[]> exportToRentabilidad(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio
-            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal) throws JRException, FileNotFoundException {
+            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal
+            ,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToRentabilidadPdf(fechaInicio,fechaFinal));
     }
-    @GetMapping("/reporte-kardex-pdf/{idVendedor}")
-    public ResponseEntity<byte[]> exportToKardex(@PathVariable String idVendedor) throws JRException, FileNotFoundException {
+    @GetMapping("/reporte-kardex-pdf/{idVendedor}/{token}")
+    public ResponseEntity<byte[]> exportToKardex(@PathVariable String idVendedor,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToKardexPdf(idVendedor));
     }
 
-    @GetMapping("/reporte-zona-pdf/{nombre}")
-    public ResponseEntity<byte[]> exportToZona(@PathVariable String nombre) throws JRException, FileNotFoundException {
+    @GetMapping("/reporte-zona-pdf/{nombre}/{token}")
+    public ResponseEntity<byte[]> exportToZona(@PathVariable String nombre,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToVentasZonaPdf(nombre));
     }
-    @GetMapping("/reporte-cliente-pdf/{fechaInicio}/{fechaFinal}/{idPersona}")
+    @GetMapping("/reporte-cliente-pdf/{fechaInicio}/{fechaFinal}/{idPersona}/{token}")
     public ResponseEntity<byte[]> exportToReporteCliente(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio
             ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal,
-            @PathVariable String idPersona) throws JRException, FileNotFoundException {
+            @PathVariable String idPersona,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToReporteClientePdf(fechaInicio,fechaFinal,idPersona));
     }
-    @GetMapping("/reporte-acomulado-venta-producto-pdf/{fechaInicio}/{fechaFinal}/{idProducto}")
+    @GetMapping("/reporte-acomulado-venta-producto-pdf/{fechaInicio}/{fechaFinal}/{idProducto}/{token}")
     public ResponseEntity<byte[]> exportToReporteAcomuladoVentaProducto(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio
             ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal,
-            @PathVariable String idProducto) throws JRException, FileNotFoundException {
+            @PathVariable String idProducto,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToReporteAcomuladoVentaProductoPdf(fechaInicio,fechaFinal,idProducto));
     }
-    @GetMapping("/reporte-venta-linea-producto-pdf/{fechaInicio}/{fechaFinal}")
+    @GetMapping("/reporte-venta-linea-producto-pdf/{fechaInicio}/{fechaFinal}/{token}")
     public ResponseEntity<byte[]> exportToReporteVentaLineaProducto(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio
-            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal) throws JRException, FileNotFoundException {
+            ,@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime fechaFinal
+            ,@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToInformeVentaLineaFechaPdf(fechaInicio,fechaFinal));
     }
-    @GetMapping("/reporte-zona-venta-fecha-pdf")
-    public ResponseEntity<byte[]> exportToZonaFecha() throws JRException, FileNotFoundException {
+    @GetMapping("/reporte-zona-venta-fecha-pdf/{token}")
+    public ResponseEntity<byte[]> exportToZonaFecha(@PathVariable String token) throws JRException, FileNotFoundException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
+        if (dataSources.getDataSourceMap().containsKey(token)) // Valida si el usuario esta autenticado. System.out.println("SESION: " + token);
+        {
+            ConstantesBD.session = token;
+        }
         return ResponseEntity.ok().headers(headers).body(IreporteService.exportToReporteVentaZonaFechaPdf());
     }
 
